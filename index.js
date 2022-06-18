6eff50394842be3abf4c194b30f897f22414545e
Jesse Okoro <okorojesse@gmail.com>
2022-06-18 19:07:28 +0100

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
-    if (req.url === '/') {
+    if (req.url === '/' || req.url === '/home') {
         fs.readFile('index.html',(err,data)=>{
             if (err) {
                 throw console.error(err);
